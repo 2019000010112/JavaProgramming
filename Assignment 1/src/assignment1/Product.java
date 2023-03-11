214dda46c00c715ad22a5165426d0cead1e97ab7
Sajedul Islam <2019000010112@seu.edu.bd>
2023-03-12 01:18:17 +0600

    practice day 2

---
+++ Assignment 1/src/assignment1/Product.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class Product {
+	public static void main(String [] args){
+		System.out.println(" id: 101,\ntitle: iphone15,\nprice: 1895 euros,\ndescription: perfect product with best image quality,\ncategory: phone,");
+		/*System.out.println("title: iphone15,");
+		System.out.println("price: 1895 euros,");
+		System.out.println("description: perfect product with best image quality,");
+		System.out.println("category: phone,");*/
+	}
+
+}
