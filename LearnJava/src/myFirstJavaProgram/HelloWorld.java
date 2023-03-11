4ff656abd348e0018cffd8fc101757db445b8313
Sajedul Islam <2019000010112@seu.edu.bd>
2023-03-12 00:57:41 +0600

    Practice Day 1
    Java Basic Practice

---
+++ LearnJava/src/myFirstJavaProgram/HelloWorld.java
@@ -0,0 +1,11 @@
+package myFirstJavaProgram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello \nSajedul Islam");
+		//System.out.print("This daily practice will help me to be a SEDT!");
+	}
+
+}
---
+++ PracticeProject 1/src/BioData/BioData.java
@@ -0,0 +1,10 @@
+package BioData;
+
+
+public class BioData {
+
+	public static void main (String [] args) {
+		System.out.println("Sajedul Islam");
+	}
+	
+}
